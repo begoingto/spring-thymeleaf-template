f247a9202bda54184cf56d18e82dd492754295da
Begoingto <begoingto.me@gmail.com>
2023-04-20 16:31:54 +0700

    update Article create can add categories🆗💯✅🎉

--- src/main/java/com/begoingto/thymeleafwebapp/controller/ArticleController.java
+++ src/main/java/com/begoingto/thymeleafwebapp/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.begoingto.thymeleafwebapp.controller;
 
 import com.begoingto.thymeleafwebapp.models.Article;
 import com.begoingto.thymeleafwebapp.models.Author;
+import com.begoingto.thymeleafwebapp.models.Category;
 import com.begoingto.thymeleafwebapp.services.ArticleService;
 import com.begoingto.thymeleafwebapp.services.CategoryService;
 import jakarta.validation.Valid;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -47,20 +49,32 @@ public class ArticleController {
     String doSaveArticle(@ModelAttribute @Valid Article article,
                          BindingResult result,
                          @RequestParam("author_id") Integer author_id,
+                         @RequestParam("category_ids") List<Integer> category_ids,
                          @RequestParam("thumbnailFile") MultipartFile file,
                          Model model){
 
+        // Filter Author by ID
         Author author =  articleService.auths().stream()
                 .filter(a -> a.getId().equals(author_id))
                 .findFirst()
                 .orElse(null);
+
+        // Filter Category in
+        System.out.println(category_ids);
+        List<Category> categories = categoryService.getCategories()
+                        .stream().filter(category -> category_ids.contains(category.id()))
+                        .toList();
+        //set author
         article.setAuthor(author);
+        // set Categories
+        article.setCategories(categories);
+
         if (result.hasErrors()){
             System.out.println(result.getFieldErrors());
             model.addAttribute("article",article);
             return "article-new";
         }
-//        articleService.save(article,file);
+        articleService.save(article,file);
         return "redirect:/article/new";
     }
 }

5d982f49e9a473244b1f007f9eb1982314cad23d
Begoingto <begoingto.me@gmail.com>
2023-11-10 01:44:01 +0700

    add env✅

--- src/test/java/com/begoingto/thymeleafwebapp/ThymeleafWebappApplicationTests.java
+++ src/test/java/com/begoingto/thymeleafwebapp/ThymeleafWebappApplicationTests.java
@@ -1,19 +1,8 @@
 package com.begoingto.thymeleafwebapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ThymeleafWebappApplicationTests {
-
-    @Test
-    void contextLoads() {
-        System.out.println("Testing context loads");
-    }
-
-    @Test
-    void test1() {
-        System.out.println("Testing test1");
-    }
 
 }
